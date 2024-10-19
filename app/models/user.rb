78f0df91ad19c1320c3e6850d8fc576e499c4a5a
Renhello112 <167634899+Renhello112@users.noreply.github.com>
2024-10-19 21:15:17 +0900

    3 commits

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 6 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'には@を含める必要があります' }
   validates :password, presence: true, length: { minimum: 6, message: 'は6文字以上で入力してください' },
                        format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }, confirmation: true
